d41dfaaf50676aaeacf0d1bb1784b9107fbdd040
revit3d <tumanezhikov@yandex.ru>
2023-11-25 23:36:47 +0300

    squash! Adding python tests

--- tests/test_ensembles.py
+++ tests/test_ensembles.py
@@ -1,8 +1,7 @@
-from src.ensembles import RandomForestMSE, GradientBoostingMSE
-import pytest
-
 import numpy as np
 from sklearn.model_selection import train_test_split
+
+from src.ensembles import RandomForestMSE, GradientBoostingMSE
 
 
 def test_random_forest():
