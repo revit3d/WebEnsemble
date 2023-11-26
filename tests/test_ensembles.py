fd63969272530a4e1603b2e9982ad1547df03316
revit3d <tumanezhikov@yandex.ru>
2023-11-26 20:29:53 +0300

    Add python tests

--- tests/test_ensembles.py
+++ tests/test_ensembles.py
@@ -3,8 +3,16 @@ from sklearn.model_selection import train_test_split
 
 from src.ensembles import RandomForestMSE, GradientBoostingMSE
 
+np.random.seed(42)
 
-def test_random_forest():
+X = np.random.randint(-10000, 10000, size=(10000, 100)) / 100
+y = np.random.randint(-10000, 10000, size=(10000,)) / 100
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8)
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, train_size=0.9)
+
+
+def test_random_forest_1():
     n_estimators = 100
     model = RandomForestMSE(
         n_estimators=n_estimators,
@@ -12,42 +20,177 @@ def test_random_forest():
         feature_subsample_size=None,
     )
 
-    X = np.random.randint(-10000, 10000, size=(10000, 100)) / 100
-    y = np.random.randint(-10000, 10000, size=(10000,)) / 100
+    try:
+        model.predict(X_val)
+    except:
+        pass
+    else:
+        raise AssertionError()
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8)
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, train_size=0.9)
+
+def test_random_forest_2():
+    n_estimators = 100
+    model = RandomForestMSE(
+        n_estimators=n_estimators,
+        max_depth=None,
+        feature_subsample_size=None,
+    )
 
     model.fit(X_train, y_train)
-    model.fit(X_train, y_train, X_val, y_val)
-
     assert len(model._models) == n_estimators
+
+
+def test_random_forest_3():
+    n_estimators = 100
+    model = RandomForestMSE(
+        n_estimators=n_estimators,
+        max_depth=None,
+        feature_subsample_size=None,
+    )
+
+    model.fit(X_train, y_train, X_val, y_val)
+    assert len(model._models) == n_estimators
+
+
+def test_random_forest_4():
+    n_estimators = 100
+    model = RandomForestMSE(
+        n_estimators=n_estimators,
+        max_depth=None,
+        feature_subsample_size=None,
+    )
+
+    model.fit(X_train, y_train)
 
     model.predict(X_val)
     y_preds = model.predict(X_test)
     assert y_preds.shape == y_test.shape
 
 
-def test_boosting():
+def test_random_forest_5():
+    n_estimators = 100
+    feature_subsample_size = 0.789
+    model = RandomForestMSE(
+        n_estimators=n_estimators,
+        max_depth=None,
+        feature_subsample_size=feature_subsample_size,
+    )
+
+    model.fit(X_train, y_train)
+
+    y_preds = model.predict(X_test)
+    assert y_preds.shape == y_test.shape
+
+
+def test_random_forest_6():
+    n_estimators = 10
+    model = RandomForestMSE(
+        n_estimators=n_estimators,
+        max_depth=5,
+        feature_subsample_size=None,
+    )
+
+    model.fit(X_train, y_train)
+
+    y_preds = model.predict(X_test)
+    assert y_preds.shape == y_test.shape
+
+
+def test_gradient_boosting_1():
     n_estimators = 100
     model = GradientBoostingMSE(
         n_estimators=n_estimators,
-        learning_rate=0.1,
         max_depth=None,
         feature_subsample_size=None,
     )
 
-    X = np.random.randint(-10000, 10000, size=(10000, 100)) / 100
-    y = np.random.randint(-10000, 10000, size=(10000,)) / 100
+    try:
+        model.predict(X_val)
+    except:
+        pass
+    else:
+        raise AssertionError()
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8)
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, train_size=0.9)
+
+def test_gradient_boosting_2():
+    n_estimators = 100
+    model = GradientBoostingMSE(
+        n_estimators=n_estimators,
+        max_depth=None,
+        feature_subsample_size=None,
+    )
 
     model.fit(X_train, y_train)
-    model.fit(X_train, y_train, X_val, y_val)
-
     assert len(model._models) == n_estimators
 
+
+def test_gradient_boosting_3():
+    n_estimators = 100
+    model = GradientBoostingMSE(
+        n_estimators=n_estimators,
+        max_depth=None,
+        feature_subsample_size=None,
+    )
+
+    model.fit(X_train, y_train, X_val, y_val)
+    assert len(model._models) == n_estimators
+
+
+def test_gradient_boosting_4():
+    n_estimators = 100
+    model = GradientBoostingMSE(
+        n_estimators=n_estimators,
+        max_depth=None,
+        feature_subsample_size=None,
+    )
+
+    model.fit(X_train, y_train)
+
     model.predict(X_val)
+    y_preds = model.predict(X_test)
+    assert y_preds.shape == y_test.shape
+
+
+def test_gradient_boosting_5():
+    n_estimators = 100
+    feature_subsample_size = 0.789
+    model = GradientBoostingMSE(
+        n_estimators=n_estimators,
+        max_depth=None,
+        feature_subsample_size=feature_subsample_size,
+    )
+
+    model.fit(X_train, y_train)
+
+    y_preds = model.predict(X_test)
+    assert y_preds.shape == y_test.shape
+
+
+def test_gradient_boosting_6():
+    n_estimators = 10
+    model = GradientBoostingMSE(
+        n_estimators=n_estimators,
+        learning_rate=0.1,
+        max_depth=5,
+        feature_subsample_size=None,
+    )
+
+    model.fit(X_train, y_train)
+
+    y_preds = model.predict(X_test)
+    assert y_preds.shape == y_test.shape
+
+
+def test_gradient_boosting_7():
+    n_estimators = 10
+    model = GradientBoostingMSE(
+        n_estimators=n_estimators,
+        learning_rate=1,
+        max_depth=5,
+        feature_subsample_size=None,
+    )
+
+    model.fit(X_train, y_train)
+
     y_preds = model.predict(X_test)
     assert y_preds.shape == y_test.shape
