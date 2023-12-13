fb64572f5fda24c7c78c5de037a0fce6c05eb720
revit3d <tumanezhikov@yandex.ru>
2023-12-13 05:05:20 +0300

    Adding module statuses to sidebar

--- src/frontend/components/Sidebar.vue
+++ src/frontend/components/Sidebar.vue
@@ -1,29 +1,53 @@
 <template>
   <aside :class="{ 'w-1/3': isOpen, 'w-0': !isOpen }" class="fixed top-0 left-0 bg-gray-800 text-white transition-all overflow-x-hidden h-full">
     <div class="ml-4">
-      <h1 class="text-2xl font-semibold tracking-wide mb-4 ml-16 mt-5">Model explorer</h1>
-      <ul class="text-blue-500">
-          <li class="mb-2"><NuxtLink to="/model/fit/1" @click="toggleSidebar">Model 1</NuxtLink></li>
-          <li class="mb-2"><NuxtLink to="/model/fit/2">Model 2</NuxtLink></li>
-          <li class="mb-2"><NuxtLink to="/model/fit/3">Model 3</NuxtLink></li>
-      </ul>
+      <h1 class="text-3xl font-semibold tracking-wide text-cyan-700 mb-8 ml-16 mt-5">Model explorer</h1>
+      <div class="text-2xl text-emerald-600 space-y-4 ml-6">
+        <ul v-for="model in models">
+          <li>
+            <button @click="redirectModel(model[0])">{{ model[1] }}</button>
+            <span :class="['dot-container', {'dot-green': isModelFitted(model[2]), 'dot-yellow': isModelFitting(model[2], model[3]), 'dot-red': !isModelFitting(model[2], model[3])}, 'ml-2']"></span>
+          </li>
+        </ul>
+      </div>
     </div>
   </aside>
 </template>
 
 <script>
-  export default {
+  export default defineNuxtComponent ({
     props: {
       isOpen: Boolean,
     },
+    async asyncData() {
+        try {
+            const response = await $fetch('http://localhost:8000/models/list');
+            return {
+                models: response.models,
+            };
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            throw error;
+        }
+    },
     methods: {
       toggleSidebar() {
-        console.log('here');
-        this.isSidebarOpen = false;
+        props.isOpen = false;
+      },
+      async redirectModel(model_uuid) {
+        await navigateTo('/model/' + model_uuid);
+        location.reload();
+      },
+      isModelFitted(is_trained) {
+        return is_trained;
+      },
+      isModelFitting(is_trained, target_name) {
+        return (target_name !== null) && !is_trained;
       },
     },
-  };
+  });
 </script>
 
 <style scoped>
+@import '@/assets/DotStyle.css';
 </style>
